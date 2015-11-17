ca63c6d3bf72f43a7340900ff497bece0737acac
Loc Tan Vo <loc.tan.vo@gmail.com>
2015-11-17 23:30:52 +0100

    Ensuring language switching in front page works

---
+++ content/themes/twolittlenomads/assets/js/language.js
@@ -0,0 +1,80 @@
+function initLanguage() {
+	setCurrentLanguage();
+
+	$('.language-english')
+		.on('click', function (event) {
+			event.preventDefault();
+			choose('english');
+		});
+
+	$('.language-german')
+		.on('click', function (event) {
+			event.preventDefault();
+			choose('german');
+		});
+
+	$('.language-norwegian')
+		.on('click', function (event) {
+			event.preventDefault();
+			choose('norwegian');
+		});
+
+	function choose(language) {
+		history.replaceState(null, document.title, "#" + language);
+		display("." + language);
+		setActive('.language-' + language);
+		setCookie("language", language)
+	}
+
+	function display(selector) {
+		$('.language').hide();
+		$(selector).fadeIn();
+	}
+
+	function setActive(selector) {
+		$('a.active').removeClass('active');
+		$(selector).addClass('active');
+	}
+	
+	function setCurrentLanguage() {
+		var language = window.location.hash.substr(1);
+		if (language === "") {
+			language = getLanguageCookie();
+		}
+		if (language.toLowerCase() === "german") {
+			choose('german');
+			return;
+		}
+		else if (language.toLowerCase() === "norwegian") {
+			choose('norwegian'); return;
+		}
+
+		choose('english');
+	}
+
+	function setLanguageCookie(value) {
+		setCookie("language", value);
+	}
+
+	function getLanguageCookie() {
+		return getCookie("language");
+	}
+
+	function setCookie(cname, cvalue) {
+		var now = new Date();
+		var expiry = new Date(now.getFullYear() + 1, 0, 1);
+		var expires = "expires=" + expiry.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; path=/; " + expires;
+	}
+
+	function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+		}
+		return "";
+	}
+} 
